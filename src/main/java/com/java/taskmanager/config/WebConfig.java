5f482d7ca9775f29afb6f4e025ce9d083a782981
Gabriella <gabriella.mabento@gmail.com>
2024-10-23 17:54:16 -0300

    fix: update CORS to allow Vercel frontend

--- src/main/java/com/java/taskmanager/config/WebConfig.java
+++ src/main/java/com/java/taskmanager/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite todos os endpoints
-                .allowedOrigins("http://localhost:5173") // Permite o frontend React
+                .allowedOrigins("http://localhost:5173", "https://task-manager-nsc1-git-main-gabriellas-projects-bb68f8bb.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os headers
                 .allowCredentials(true); // Permite envio de cookies (se necessário)
