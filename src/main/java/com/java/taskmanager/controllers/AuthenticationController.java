89c5b43b1a8483efea07d22e77df600e6438332d
Gabriella <gabriella.mabento@gmail.com>
2025-01-20 15:30:44 -0300

    feat: add AuthenticationController and AuthenticationDTO

---
+++ src/main/java/com/java/taskmanager/controllers/AuthenticationController.java
@@ -0,0 +1,17 @@
+package com.java.taskmanager.controllers;
+
+import com.java.taskmanager.dtos.AuthenticationDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+//    @PostMapping("/register")
+//    public ResponseEntity register(@Valid @RequestBody AuthenticationDTO authenticationDTO){
+//
+//    }
+}
---
+++ src/main/java/com/java/taskmanager/dtos/AuthenticationDTO.java
@@ -0,0 +1,4 @@
+package com.java.taskmanager.dtos;
+
+public record AuthenticationDTO(String username, String password) {
+}
